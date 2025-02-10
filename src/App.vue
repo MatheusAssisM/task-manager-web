8b6932d7e8ce575143946220075b933c6d2ce8d3
MatheusAssisLend <matheus.melo@lend.tech>
2025-02-10 00:11:54 -0300

    Add loading spinner during authentication initialization

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <router-view />
+  <router-view v-if="!loading" />
+  <div v-else class="fullscreen flex flex-center">
+    <q-spinner-dots size="40px" color="primary" />
+  </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useAuthStore } from './stores/auth'
 
 const authStore = useAuthStore()
+const loading = ref(true)
 
-onMounted(() => {
-  authStore.initializeAuth()
+onMounted(async () => {
+  try {
+    await authStore.initializeAuth()
+  } finally {
+    loading.value = false
+  }
 })
 </script>
